9bc7f6667624a3abdf4728c94264c412364b2fee
Dave Perlman <daveperlman89@me.com>
2017-11-17 15:24:01 -0500

    background fade, web safe images

--- index.php
+++ index.php
@@ -24,22 +24,28 @@
 				<a href="https://goo.gl/maps/KPBa8RznVHD2" target="_blank">
 					<i class="fa fa-map"></i>
 				</a>
+				<span>Directions</span>
 			</li>
 			<li>
 				<a href="tel:207-440-4576">
 					<i class="fa fa-phone"></i>
 				</a>
+				<span>Phone</span>
 			</li>
 			<li>
 				<a href="mailto:service@daveperlmanestateimages.com">
 					<i class="fa fa-envelope"></i>
 				</a>
+				<span>Email</span>
 			</li>
 		</ul>
 	</header>
 	</div>
-	<div class="banner">
-		<section id="text">
+	<div class="carousel">
+		<div class="inner"></div>
+		<div class="banner"></div>
+	</div>
+	<!-- <section id="text">
 		<h1>dave perlman estate images</h1>
 		<ul id="textSlides">
 			<li>
@@ -56,31 +62,61 @@
 				<p>Find out how to make your first impression shine</p>
 				<a href="mailto:service@daveperlmanestateimages.com" id="cta">Here</a>
 			</li>
-		</ul>
+		</ul> 
+	</section> -->
+
+	<section class="about">
+		<div class="col">
+			<h2>Quality Images</h2>
+			<img src="_img/camera.png" alt="">
+				<p>A quality photograph tells a lot about a property, it is often times the first impression a buyer has of a new home or commercial location. To us, it only makes sense to have a listing that stands out and highlights the assets of a property.</p>
+		</div>
+		<div class="col">
+			<h2>Perspective Shots</h2>
+			<img src="_img/drone.png" alt="">
+				<p>A new angle often makes a compelling photograph. With the power of flight, we're able to capture the most compelling images that offer an unique perspective. Aerial photography is becoming an industry standard and is a fantastic way to offer more to your buyer or seller.</p>
+		</div>
+		<div class="connect">
+			<h3>Make a Good First Impression</h3>
+				<p>With the abundance of homebuying experiences starting on a computer or mobile device, capturing a buyer's attention with high quality images of their new home is a must.</p>
+				<p>Find out how to make your first impression shine</p>
+				<a href="mailto:service@daveperlmanestateimages.com" id="cta">here</a>
+		</div>
 	</section>
-	<ul class="social">
+	<footer>
+		<ul class="social">
 			<li class="fb">
 				<a href="https://www.facebook.com/luxurypropertyphotos/">
 					<i class="fa fa-facebook"></i>
 				</a>
+				<span>Facebook</span>
 			</li>
 			<li class="yt">
 				<a href="https://www.youtube.com/channel/UCxjPmBFfLNuTHM9bGABY_0g">
 					<i class="fa fa-youtube"></i>
 				</a>
+				<span>Youtube</span>
 			</li>
 			<li class="ig">
 				<a href="https://www.instagram.com/luxurypropertyimages">
 					<i class="fa fa-instagram"></i>
 				</a>
+				<span>Instagram</span>
 			</li>
 			<li class="fk">
 				<a href="https://www.flickr.com/people/153207331@N04/">
 					<i class="fa fa-flickr"></i>
 				</a>
+				<span>Flikr</span>
 			</li>
 		</ul>
-	</div>
+		<div class="copy">
+			<p>&#9400; 2015</p>
+			<p>
+				<a href="http://www.refined-tech.com">A <em>Refined Technologies</em> site</a>
+			</p>
+		</div>
+	</footer>
 	<script src="main.js"></script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -7,7 +7,37 @@
 	var current = 0;
 	var hovering = false;
 
+	var imageSet1 = ['_img/1.jpg','_img/3.jpg','_img/5.jpg','_img/7.jpg'];
+	var currentImageSet1 = 0;
+	var imageSet2 = ['_img/2.jpg','_img/4.jpg','_img/6.jpg','_img/8.jpg'];
+	var currentImageSet2 = 0;
 	$(init);
+	setInterval(changeBackgroundImages, pace * 2000);
+	
+
+	function changeBackgroundImages() {
+    	img1Fade();
+    	setTimeout(img2Fade, 4000);
+	}
+
+	function img1Fade(){
+
+	    $('.banner').fadeOut(2000, function(){$('.banner').css({"background-image": 'url(' + imageSet1[++currentImageSet1] + ')'})});
+	    $('.inner').fadeIn(2000);
+	    if (currentImageSet1 >= imageSet1.length - 1) {
+
+	            currentImageSet1 -= imageSet1.length;
+	        };
+	}
+
+	function img2Fade(){
+	    $('.inner').fadeOut(2000, function(){$('.inner').css({"background-image": 'url(' + imageSet2[++currentImageSet2] + ')'})});
+	    $('.banner').fadeIn(2000);
+	    if (currentImageSet2 >= imageSet2.length - 1) {
+
+	            currentImageSet2 -= imageSet2.length;
+	        };
+	}
 
 	function init(){
 		var $parent = $(targetParentName);
